188602540d3dfa1cd64c6791bae405aef669b7c7
Pavel Kindruk <kinpa200296@yandex.ru>
2016-05-12 09:29:28 +0300

    version 0.003
    Implemented commands loading

--- cmdpy.py
+++ cmdpy.py
@@ -2,16 +2,32 @@ import readline
 import pywin
 from cmd2 import Cmd
 import logging
+import sys
 from core import inspect_package
 
 
 class CmdPy(Cmd):
     def __init__(self, *args, **kwargs):
-        Cmd.__init__(self, *args, **kwargs)
         self.__logger = logging.getLogger("cmdpy.CmdPy")
+        self.setup_commands()
+        Cmd.__init__(self, *args, **kwargs)
+
+    def setup_commands(self):
         self.__logger.debug('Starting commands loading')
-        self.commands = inspect_package('commands')
+        commands = inspect_package('commands')
         self.__logger.debug('Finished commands loading')
+        for cmd in commands:
+            if hasattr(Cmd, cmd.__name__):
+                if cmd.is_alias:
+                    self.__logger.warn('Can\'t add alias {name}. Name already exists'.format(name=cmd.__name__))
+                else:
+                    self.__logger.error('Can\'t add command {name}. Name already exists'.format(name=cmd.__name__))
+            else:
+                setattr(Cmd, cmd.__name__, cmd)
+                if cmd.is_alias:
+                    self.__logger.debug('Added alias {name}'.format(name=cmd.__name__))
+                else:
+                    self.__logger.debug('Added command {name}'.format(name=cmd.__name__))
 
 
 if __name__ == '__main__':
@@ -19,5 +35,11 @@ if __name__ == '__main__':
     logger.debug('Creating app instance')
     app = CmdPy()
     logger.debug('Starting cmd loop')
-    app.cmdloop()
+    exit_code = 0
+    try:
+        app.cmdloop()
+    except:
+        exit_code = -1
+        logger.error('Unhandled exception occurred. ' + sys.exc_info()[0])
     logger.debug('Cmd loop finished')
+    sys.exit(exit_code)
--- core/loader.py
+++ core/loader.py
@@ -28,14 +28,38 @@ def inspect_module(name):
                 try:
                     commands.extend(tmp)
                 except TypeError:
-                    logger.error('Module {name} inspection returned {res}'.format(name=name, res=repr(tmp)))
+                    logger.error('Command {name} inspection returned {res}'.format(name=name, res=repr(tmp)))
     logger.debug('Done inspecting module {name}'.format(name=name))
     return commands
+
+
+def cmd_wrapper(func, name, cmd_name, is_alias):
+    def f(*args, **kwargs):
+        return func(*args, **kwargs)
+    f.__name__ = name
+    f.cmd_name = cmd_name
+    f.is_alias = is_alias
+    if is_alias:
+        logger.debug('Loaded alias {alias} for command {cmd}'.format(alias=name, cmd=cmd_name))
+    else:
+        logger.debug('Loaded {func} for command {cmd}'.format(func=name, cmd=cmd_name))
+    return f
+
 
 def make_commands(func):
     if not (hasattr(func, '_cmd_name') and hasattr(func, '_cmd_prefix') and hasattr(func, '_cmd_aliases')):
         logger.error('Invalid command function {func}'.format(func=repr(func)))
         raise ValueError('Invalid command function')
+    cmd_name = getattr(func, '_cmd_name')
+    cmd_prefix = getattr(func, '_cmd_prefix')
+    cmd_aliases = getattr(func, '_cmd_aliases')
+    logger.debug('Loading command {cmd} from function {func}'.format(cmd=cmd_name, func=func.__name__))
     commands = []
-    # add command definition
+    commands.append(cmd_wrapper(func, '_'.join([cmd_prefix, cmd_name]), cmd_name, False))
+    try:
+        for alias in cmd_aliases:
+            commands.append(cmd_wrapper(func, '_'.join([cmd_prefix, alias]), cmd_name, True))
+    except TypeError:
+        logger.debug('No aliases for {cmd} were found'.format(cmd=cmd_name))
+        pass
     return commands
